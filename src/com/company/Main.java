acd34415376d931ded3534f3db60378aaf22295a
Sean <36829049+stran556@users.noreply.github.com>
2022-09-23 15:10:25 -0500

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,15 +14,6 @@ public class Main {
                      61, 53, 45, 37, 29, 21, 13, 5,
                      63, 55, 47, 39, 31, 23, 15, 7 };
 
-        //final permutation
-        int FP[] = { 40, 8, 48, 16, 56, 24, 64, 32,
-                     39, 7, 47, 15, 55, 23, 63, 31,
-                     38, 6, 46, 14, 54, 22, 62, 30,
-                     36, 4, 44, 12, 52, 20, 60, 28,
-                     35, 3, 43, 11, 51, 19, 59, 27,
-                     34, 2, 42, 10, 50, 18, 58, 26,
-                     33, 1, 41, 9,  49, 17, 57, 25 };
-
         //expansion permutation
         int E[] = { 32, 1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,
                     8,  9,  10, 11, 12, 13, 12, 13, 14, 15, 16, 17,
@@ -92,11 +83,51 @@ public class Main {
                     1,  15, 23, 26, 5,  18, 31, 10,
                     2,  8,  24, 14, 32, 27, 3,  9,
                     19, 13, 30, 6,  22, 11, 4,  25 };
+
+        //final permutation
+        int FP[] = { 40, 8, 48, 16, 56, 24, 64, 32,
+                39, 7, 47, 15, 55, 23, 63, 31,
+                38, 6, 46, 14, 54, 22, 62, 30,
+                36, 4, 44, 12, 52, 20, 60, 28,
+                35, 3, 43, 11, 51, 19, 59, 27,
+                34, 2, 42, 10, 50, 18, 58, 26,
+                33, 1, 41, 9,  49, 17, 57, 25 };
+
+
+
+        String asciiToBinary(String s) {
+            byte[] bytes = s.getBytes();
+            StringBuilder binary = new StringBuilder();
+            for (byte b : bytes) {
+                int val = b;
+                for (int i = 0; i < 8; i++) {
+                    binary.append((val & 128) == 0 ? 0 : 1);
+                    val <<= 1;
+                }
+                //binary.append(' ');
+            }
+            //System.out.println("'" + s + "' to binary: " + binary);
+            return binary + "";
+        }
+
+        String binaryToAscii(){
+            return "";
+        }
+
+        String roundRobbin() {
+            return "";
+        }
     }
 
 
-
     public static void main(String[] args) {
+        String plaintext = "honolulu";
+        String key = "madiisen";
+        DES cipher = new DES();
+        System.out.println(cipher.asciiToBinary(plaintext));
+        System.out.println(cipher.asciiToBinary(key));
+
+
 
     }
 }
