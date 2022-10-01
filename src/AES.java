1f63cda3594ac463f3d1085b0cf83bcf24ec90ce
Sean <36829049+stran556@users.noreply.github.com>
2022-10-01 18:00:12 -0500

    Update AES.java

--- src/AES.java
+++ src/AES.java
@@ -1,3 +1,49 @@
 package company;
 
+public class AES {
 
+    String rotWord(){
+        return "";
+    }
+
+    String subWord(){
+        return "";
+    }
+
+    String roundConstant(){
+        return "";
+    }
+
+    String keySchedule(){
+        return "";
+    }
+
+    String addRoundKey(){
+        return "";
+    }
+
+    String subBytes(){
+        return "";
+    }
+
+    String shiftRows(){
+        return "";
+    }
+
+    String mixColumns(){
+        return "";
+    }
+
+    String round(){
+        return "";
+    }
+
+    String encrypt(){
+        return "";
+    }
+
+    String decrypt(){
+        return "";
+    }
+    
+}
