2d96259ca5e060e2c4a8e332279c3835c98726d3
Sean <36829049+stran556@users.noreply.github.com>
2022-10-01 23:59:02 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -385,6 +385,147 @@ public class Main {
     }
 
     private static class AES {
+                        // 0     1     2     3     4     5     6     7     8     9     a     b     c     d     e     f
+        int sbox[][] = { { 0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76 },
+                         { 0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0 },
+                         { 0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15 },
+                         { 0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75 },
+                         { 0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84 },
+                         { 0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf },
+                         { 0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8 },
+                         { 0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2 },
+                         { 0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73 },
+                         { 0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb },
+                         { 0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79 },
+                         { 0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08 },
+                         { 0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a },
+                         { 0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e },
+                         { 0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf },
+                         { 0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 } };
+
+        String[][] stringToBlock(String input){ //COLUMN-MAJOR
+            String block[][] = new String[4][4];
+            int counter = 0;
+            for(int i = 0; i < 4; i++){
+                for(int ii = 0; ii < 4; ii++){
+                    block[ii][i] = input.substring(0 + counter, 2 + counter);
+                    counter = counter + 2;
+                }
+            }
+
+            for(int i = 0; i < 4; i++){
+                for(int ii = 0; ii < 4; ii++){
+                    System.out.print(block[ii][i] + " ");
+                }
+                System.out.println();
+            }
+            return block;
+        }
+
+        String blockToString(String[][] block){
+            String text = "";
+            for(int i = 0; i < 4; i++){
+                for(int ii = 0; ii < 4; ii++){
+                    text = text + block[i][ii];
+                }
+            }
+            return text;
+        }
+
+        String sbox(String input){
+            int num = sbox[Integer.parseInt(input.substring(0, 1), 16)][Integer.parseInt(input.substring(1), 16)];
+
+            return Integer.toHexString(num);
+        }
+
+
+        String[] rotWord(String input[]){
+            //for(int i = 0; i < 4; i++){
+            //    System.out.println(input[i]);
+            //}
+
+            String temp = input[0];
+            input[0] = input[1];
+            input[1] = input[2];
+            input[2] = input[3];
+            input[3] = temp;
+
+            //for(int i = 0; i < 4; i++){
+            //    System.out.println(input[i]);
+            //}
+
+            return input;
+        }
+
+        String subWord(String input[]){
+            for(int i = 0; i < 4; i++){
+                input[i] = sbox(input[i]);
+                System.out.println(input[i]);
+            }
+
+            return "";
+        }
+
+        String rCon(){
+            return "";
+        }
+
+        String keySchedule(String key){
+            int len = key.length() / 2; //byte length
+            String keySchedule[];
+            String block[][] = stringToBlock(key.toLowerCase());
+            int round;
+
+            if(len == 32)
+                round = 14;
+            else if(len == 24)
+                round = 12;
+            else //len = 16
+                round = 10;
+
+            for(int i = 0; i < 1; i++){
+                subWord(rotWord(block[3]));
+                //rotWord
+                //subWord
+                //rCon
+            }
+
+
+
+            return "";
+        }
+
+        String addRoundKey(){
+            return "";
+        }
+
+        String subBytes(){
+            return "";
+        }
+
+        String shiftRows(){
+            return "";
+        }
+
+        String mixColumns(){
+            return "";
+        }
+
+        String round(){
+            return "";
+        }
+
+        String encrypt(String text, String key){
+
+
+            keySchedule(key);
+            return "";
+        }
+
+        String decrypt(){
+            return "";
+        }
+
 
 
     }
@@ -392,17 +533,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-        //64-bit hexadecimal
-        String text = "4D61646973656E53";
-        String key =  "4348414D50494F4E";
 
-        //For 3DES
-        String key2 = "4C4F4E47484F524E";
-        String key3 = "6669726562616C6C";
 
-        DES des = new DES();
+
         try {
-            String ciphertext = des.encrypt(text, key);
             Scanner cs = new Scanner(System.in);
             System.out.println("|--[ALG]");
             System.out.println("|    [1]DES \n|    [2]AES ");
@@ -410,6 +544,7 @@ public class Main {
             int enc = cs.nextInt();
 
             if(enc == 1) { //DES
+                DES des = new DES();
                 System.out.println("|------[DES]");
                 System.out.println("|        [1]FILE \n|        [2]MANUAL ");
                 Scanner sc = new Scanner(System.in);
@@ -417,6 +552,15 @@ public class Main {
                 int typeInput = sc.nextInt();
 
                 if (typeInput == 1) { //DES/FILE
+                    //64-bit hexadecimal
+                    String text = "4D61646973656E53";
+                    String key =  "4348414D50494F4E";
+
+                    //For 3DES
+                    String key2 = "4C4F4E47484F524E";
+                    String key3 = "6669726562616C6C";
+                    String ciphertext = des.encrypt(text, key);
+
                     System.out.println("            Hex-------------   Ascii---");
                     System.out.println("Plaintext:  " + text + "   " + des.binaryToAscii(des.hexToBinary(text)));
                     System.out.println("Key:        " + key + "   " + des.binaryToAscii(des.hexToBinary(key)));
@@ -500,6 +644,14 @@ public class Main {
                 }
             }
             if(enc == 2){ //AES...
+                AES aes = new AES();
+                String text = "4D41444953454E534B494E4E45523036";
+                String key = "5445584153564F4C4C455942414C4C31";
+                System.out.println(aes.encrypt(text, key));
+
+                aes.blockToString(aes.stringToBlock(key));
+
+
                 System.out.println("AES");
                 System.out.println("In progress...");
             }
