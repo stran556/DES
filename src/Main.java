5d6fdbe0d31e9b894a088dfcba580b994dcb4b18
Sean <36829049+stran556@users.noreply.github.com>
2022-10-02 19:55:09 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -623,6 +623,35 @@ public class Main {
         }
 
         String[][] shiftRows(String block[][]){
+
+            String temp = "";
+            String temp2 = "";
+            temp = block[1][0];
+            block[1][0] = block[1][1];
+            block[1][1] = block[1][2];
+            block[1][2] = block[1][3];
+            block[1][3] = temp;
+
+            temp = block[2][0];
+            temp2 = block[2][1];
+            block[2][0] = block[2][2];
+            block[2][2] = temp;
+            block[2][1] = block[2][3];
+            block[2][3] = temp2;
+
+            temp = block[3][3];
+            block[3][3] = block[3][2];
+            block[3][2] = block[3][1];
+            block[3][1] = block[3][0];
+            block[3][0] = temp;
+
+            System.out.println("-shiftrows-");
+            for(int i = 0; i < 4; i++){ //column-major print
+                for(int ii = 0; ii < 4; ii++){
+                    System.out.print(block[i][ii] + " ");
+                }
+                System.out.println();
+            }
             return block;
         }
 
@@ -636,20 +665,15 @@ public class Main {
 
             String builder = "";
             addRoundKey = subBytes(roundKey); //subBytes
+
+
             for(int i = 0; i < 16; i++){ //array to string
                 builder = builder + addRoundKey[i];
             }
             String block[][] = stringToBlock(builder);
-
-
-            for(int i = 0; i < 4; i++){ //column-major print
-                for(int ii = 0; ii < 4; ii++){
-                    System.out.print(block[i][ii] + " ");
-                }
-                System.out.println();
-            }
-
             block = shiftRows(block);
+
+
 
             /*
             n = subBytes(roundKey)
@@ -657,9 +681,10 @@ public class Main {
             mixColumns(n)
             addRoundKey = xor(n, keyBlock)
              */
-            System.out.println();
+
+            System.out.print("\nKey: ");
             for(int j = 0; j < 16; j++){
-                //System.out.print(addRoundKey[j]);
+                System.out.print(keyBlock[j]);
             }
 
 
